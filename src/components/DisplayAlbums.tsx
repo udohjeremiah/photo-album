b22f444ef84b8af1d13cd54e2675ea85c349737f
Udoh Jeremiah <udohjeremiah@icloud.com>
2024-03-15 00:18:04 +0100

    Fix: Use external library for masonry layout feature

--- src/components/DisplayAlbums.tsx
+++ src/components/DisplayAlbums.tsx
@@ -10,6 +10,7 @@ import Link from "next/link";
 // Dependencies
 import { useUser } from "@clerk/nextjs";
 import { FolderXIcon, LoaderIcon } from "lucide-react";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
 // Actions
 import { deleteAlbum } from "@/actions/deleteAlbum";
@@ -62,42 +63,38 @@ export default function DisplayAlbums({ albums }: { albums: AlbumProps[] }) {
   };
 
   return (
-    <div
-      className={cn(
-        "grid grid-cols-1 gap-4",
-        "md:grid-cols-2",
-        "lg:grid-cols-3",
-      )}
-    >
-      {albums.map((album) => (
-        <Card key={album.path}>
-          <CardHeader>
-            <CardTitle>{album.name} Album</CardTitle>
-            <CardDescription>
-              Your {album.name} images in one place.
-            </CardDescription>
-          </CardHeader>
-          <CardFooter className="flex justify-between gap-4">
-            <Button
-              variant="destructive"
-              disabled={isDeleting[album.name]}
-              onClick={() => {
-                deleteAlbumFromAccount(user?.id, album.name);
-              }}
-            >
-              {isDeleting[album.name] ? (
-                <LoaderIcon className="mr-2 h-4 w-4 animate-spin" />
-              ) : (
-                <FolderXIcon className="mr-2 h-4 w-4" />
-              )}
-              Delete
-            </Button>
-            <Button asChild>
-              <Link href={`/albums/${album.name}`}>View Album</Link>
-            </Button>
-          </CardFooter>
-        </Card>
-      ))}
-    </div>
+    <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 768: 2, 1024: 3 }}>
+      <Masonry gutter="32px">
+        {albums.map((album) => (
+          <Card key={album.path}>
+            <CardHeader>
+              <CardTitle>{album.name} Album</CardTitle>
+              <CardDescription>
+                Your {album.name} images in one place.
+              </CardDescription>
+            </CardHeader>
+            <CardFooter className="flex justify-between gap-4">
+              <Button
+                variant="destructive"
+                disabled={isDeleting[album.name]}
+                onClick={() => {
+                  deleteAlbumFromAccount(user?.id, album.name);
+                }}
+              >
+                {isDeleting[album.name] ? (
+                  <LoaderIcon className="mr-2 h-4 w-4 animate-spin" />
+                ) : (
+                  <FolderXIcon className="mr-2 h-4 w-4" />
+                )}
+                Delete
+              </Button>
+              <Button asChild>
+                <Link href={`/albums/${album.name}`}>View Album</Link>
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </Masonry>
+    </ResponsiveMasonry>
   );
 }
